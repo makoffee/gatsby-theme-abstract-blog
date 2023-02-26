f067aafdeedc0dbabbaef94c96c585efd1752d08
makoffee <makoffee@antidata.net>
2023-02-27 00:20:02 +0100

    Change name of HomepageBanner to TextBannerContent

--- src/post.js
+++ src/post.js
@@ -37,7 +37,7 @@ export const query = graphql`
         ...HomepageProductListContent
         ...RichTextBlockContent
         ...CtaImageBlockContent
-        ...HeaderBannerContent
+        ...TextBannerContent
         ...VideoEmbedContent
       }
       author {
